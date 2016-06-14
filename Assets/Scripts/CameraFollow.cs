6ec75015900a18cbc89177d52a909001132d1179
Ted Mader <tlmader@live.com>
2016-06-14 18:05:35 -0500

    Add CameraFollow script and update Main Camera prefab

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Original post with image here: http://unity3diy.blogspot.com/2015/02/unity-2d-camera-follow-script.html
+
+public class CameraFollow : MonoBehaviour
+{
+	public float interpVelocity;
+	public float interpVelocityModifier = 5f;
+	public float minDistance;
+	public float followDistance;
+	public GameObject target;
+	public Vector3 offset;
+	Vector3 targetPos;
+
+	// Use this for initialization
+	void Start()
+	{
+		targetPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (target)
+		{
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+			interpVelocity = targetDirection.magnitude * interpVelocityModifier;
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+			transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+		}
+	}
+}
