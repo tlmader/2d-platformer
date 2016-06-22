4e60ec3ab4e86c013d1964daf211e6c662236970
Ted Mader <tlmader@live.com>
2016-06-22 18:44:15 -0500

    Add prefabs and PlayerControl script and update TestGround prefab

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+	public float speed = 50f;
+	public float jumpPower = 150f;
+
+	public bool grounded;
+
+	private Rigidbody2D body;
+
+	void Start()
+	{
+		body = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+		body.AddForce((Vector2.right * speed) * h);
+	}
+}
