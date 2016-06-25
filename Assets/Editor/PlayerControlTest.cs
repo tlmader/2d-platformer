7eaca6145b991ba1c1c9b84514406c2122458e78
Ted Mader <tlmader@live.com>
2016-06-25 17:09:31 -0500

    Add first working unit test

---
+++ Assets/Editor/PlayerControlTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class PlayerControlTest {
+
+    [Test]
+    public void Update_JumpWhileGrounded()
+    {
+        var found = GameObject.FindGameObjectWithTag("Player");
+		var expected = "TestCharacter";
+		var actual = found.name;
+
+        Assert.AreEqual(expected, actual);
+    }
+}
